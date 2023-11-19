4ba746309a83a5c341435dedb5315d20d6bb598e
Nurlan <124293340+Nurlan98Is@users.noreply.github.com>
2023-11-20 00:29:24 +0700

    Add files via upload

---
+++ scr/FrontSide/City/City.jsx
@@ -0,0 +1,10 @@
+import style from './City.module.css'
+export default function City(props){
+    
+    return(
+        <div className ={style.city_conteiner}>
+            <p className ={style.city}>{props.city === '' ? "City..." : props.city}</p>
+            <p className ={style.description}>{props.description}</p>
+        </div>
+    )
+}
