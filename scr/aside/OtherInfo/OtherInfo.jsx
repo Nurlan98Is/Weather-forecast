acf13f3cc54455fe972ea3142c658ebbdda58a88
Nurlan <124293340+Nurlan98Is@users.noreply.github.com>
2023-11-20 00:27:29 +0700

    Add files via upload

---
+++ scr/aside/OtherInfo/OtherInfo.jsx
@@ -0,0 +1,21 @@
+import style from './OtherInfo.module.css'
+import Barometr from 'a:/projects/App weather forecast/Weather_forecast/src/assets/barometr.png'
+import Wind from 'a:/projects/App weather forecast/Weather_forecast/src/assets/wind.png'
+import Visibility from 'a:/projects/App weather forecast/Weather_forecast/src/assets/visibility.png'
+import Humidity from 'a:/projects/App weather forecast/Weather_forecast/src/assets/humidity.png'
+export default function OtherInfo(props){
+    return(
+            <div className={style.otherinfo_conteiner}>
+                <hr />
+                <p className ={style.wind}><img src={Wind} alt="" />{props.windSpeed}m/s SW</p>
+                <hr/>
+                <p className={style.humidity}><img src={Humidity} alt="Humidity" />Humidity:{props.humidity}%</p>
+                <hr />
+                <p className ={style.pressure}><img src={Barometr} alt="Pressure" />Pressure:{props.pressure} hPA</p>
+                <hr />
+                <p className={style.visibility}><img src={Visibility} alt="Visibility" />Visibility: {props.visibility}Meters</p>
+                <hr />
+            </div>
+            
+    )
+}
